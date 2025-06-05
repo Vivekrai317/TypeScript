93b82482160b86428b08c9505f67bd9f05761cd5
Vivekrai317 <vivekrai317@gmail.com>
2025-06-05 14:57:36 +0530

    basic types

---
+++ L1/app.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 20;
+var n2 = 30;
+console.log(add(n1, n2));
---
+++ L1/app.ts
@@ -0,0 +1,10 @@
+function add(num1:number, num2:number){
+    return num1+num2
+}
+// basically 3 data types -> number , boolean and string
+const n1 = 20;
+const n2 = 30;
+
+const printResult = true;
+
+console.log(add(n1,n2));
