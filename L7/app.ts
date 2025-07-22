e9dc800dfbed65ee8a113ccbb0a7aa7674869608
Vivekrai317 <vivekrai317@gmail.com>
2025-07-22 23:10:23 +0530

    jhjh

--- L7/app.ts
+++ L7/app.ts
@@ -20,4 +20,42 @@ class BottleMaker{
 
 new BottleMaker("Milton",1200)
 
- 
+class abcd{
+    name = "tv";
+    changeName(){
+        this.name = "vv";
+    }
+}
+
+// access modifiers -> public private protected
+let a = new abcd();
+
+//if we dont want anything to change then add readonly
+class User{
+    constructor(public readonly name:string){
+
+    }
+    changeName(){
+        // this.name="new name" // now shows error because of readonly
+    }
+}
+
+// if add ? then we can add optional parameters
+// eg. public name:string public gender?:string
+
+class User2{
+    constructor(public _name:string, public _age:number){
+
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(name:string){
+        this._name=name;
+    }
+}
+
+// if static keyword is used then we dont need to create a new instance and can be accessed directly ie bina instance bnaye
+
+let x = abcd;
